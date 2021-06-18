ff65aaf6ff4cca88c2e4b96356ef059454159e9f
naicop <nrombiola@gmail.com>
2021-06-18 20:14:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 let randomlinks=new Array()
 
-randomlinks[0]="http://freewarejava.com"
-randomlinks[1]="http://javascriptkit.com"
-randomlinks[2]="http://dynamicdrive.com"
-randomlinks[3]="http://cnn.com"
-randomlinks[4]="http://www.geocities.com"
+randomlinks[0]="https://forms.gle/BfiWAUyzH9EaHfdH7"
+randomlinks[1]="https://forms.gle/7AUVZDLR9dQ26qXa6"
+randomlinks[2]="https://forms.gle/wPgZzA8Lm6AMb8M56"
 
 function randomlink(){
 window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
