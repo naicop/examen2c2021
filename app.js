f1585e621785e8482488b8ef7667ddd98c4d9e38
naicop <nrombiola@gmail.com>
2021-06-19 11:32:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let randomlinks=new Array()
 
-randomlinks[0]="https://forms.gle/BfiWAUyzH9EaHfdH7"
-randomlinks[1]="https://forms.gle/7AUVZDLR9dQ26qXa6"
+randomlinks[0]="https://forms.gle/yN6ADk5pNAbSovmz5"
+randomlinks[1]="https://forms.gle/9HgZ2uJojo7uwsp48"
 
 function randomlink(){
 window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
